d857caeea891db307a4aaa9d1bc8238527dfec30
Łukasz Juroszek <lukjuroszek@gmail.com>
2017-03-10 23:39:20 +0100

    Add sample connection and test dapper sample query

--- DapperTestCRUD/DapperTestCRUD/Program.cs
+++ DapperTestCRUD/DapperTestCRUD/Program.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Dapper;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +13,18 @@ namespace DapperTestCRUD
     {
         static void Main(string[] args)
         {
+            using (var dBConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["msSQLDataBase"].ConnectionString))
+            {
+                dBConnection.Open();
+                var query = "Select * from Produkt";
+                var resultQuery = dBConnection.Query<Produkt>(query);
+                foreach (var item in resultQuery)
+                {
+                    if (( item.Jednostka_Produktu != "sztuki" ))
+                        Console.WriteLine(item);
+                    Console.WriteLine(item);
+                }
+            }
         }
     }
 }
